6ff650bb0b42776c30d3777d9c6db8d965cd2199
stefanteau7 <stefanteau7@gmail.com>
2023-09-01 20:37:29 -0700

    keep user logged in

--- share_app/src/providers/UserContext.tsx
+++ share_app/src/providers/UserContext.tsx
@@ -1,4 +1,7 @@
-import React, {createContext, useState, useContext} from 'react';
+import {onAuthStateChanged} from 'firebase/auth';
+import React, {createContext, useState, useContext, useEffect} from 'react';
+import {auth} from '../../firebaseConfig';
+import {User} from 'firebase/auth';
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
@@ -7,12 +10,28 @@ interface UserProviderProps {
 }
 
 interface UserContextType {
-  userInfo: any | null;
+  userInfo: User | null;
   setUserInfo: React.Dispatch<React.SetStateAction<any | null>>;
 }
 
 export const UserProvider: React.FC<UserProviderProps> = ({children}) => {
-  const [userInfo, setUserInfo] = useState(null);
+  const [userInfo, setUserInfo] = useState<User | null>(null);
+
+  useEffect(() => {
+    // Set up the listener for authentication state changes
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      if (user) {
+        // User is signed in.
+        setUserInfo(user);
+      } else {
+        // User is signed out.
+        setUserInfo(null);
+      }
+    });
+
+    // Cleanup the listener on component unmount
+    return () => unsubscribe();
+  }, []);
 
   return (
     <UserContext.Provider value={{userInfo, setUserInfo}}>
