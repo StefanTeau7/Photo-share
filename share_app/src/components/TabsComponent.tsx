3cdf68216342adebe936146a7f031b5900eff72f
stefanteau7 <stefanteau7@gmail.com>
2023-09-02 14:18:09 -0700

    add bottom nav icons

--- share_app/src/components/TabsComponent.tsx
+++ share_app/src/components/TabsComponent.tsx
@@ -1,9 +1,18 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import FavoriteImagesScreen from '../screens/FavoriteImagesScreen';
 import UnsplashGridView from '../screens/UnsplashGridView';
 
 const Tab = createBottomTabNavigator();
+
+const UnsplashIcon = () => (
+  <Ionicons name={'images-outline'} size={20} color={'#007AFF'} />
+);
+
+const FavoriteIcon = () => (
+  <Ionicons name={'heart-outline'} size={20} color={'#007AFF'} />
+);
 
 const TabsComponent = () => {
   return (
@@ -11,12 +20,18 @@ const TabsComponent = () => {
       <Tab.Screen
         name="UnsplashGridView"
         component={UnsplashGridView}
-        options={{title: 'Unsplash Images'}}
+        options={{
+          title: 'Unsplash Images',
+          tabBarIcon: UnsplashIcon,
+        }}
       />
       <Tab.Screen
         name="FavoriteImagesScreen"
         component={FavoriteImagesScreen}
-        options={{title: 'Favorite Images'}}
+        options={{
+          title: 'Favorite Images',
+          tabBarIcon: FavoriteIcon,
+        }}
       />
     </Tab.Navigator>
   );
