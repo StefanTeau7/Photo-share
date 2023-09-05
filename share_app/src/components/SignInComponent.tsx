12b0b9dbd9b0be90a88a5a67cdc93942326b64d0
stefanteau7 <stefanteau7@gmail.com>
2023-09-05 15:06:09 -0700

    remove server url need

--- share_app/src/components/SignInComponent.tsx
+++ share_app/src/components/SignInComponent.tsx
@@ -1,17 +1,10 @@
 import {GoogleSigninButton} from '@react-native-google-signin/google-signin';
-import React, {useRef, useState} from 'react';
-import {Animated, Button, TextInput, View} from 'react-native';
+import React, {useRef} from 'react';
+import {Animated, Button, View} from 'react-native';
 import {styles} from '../styles/styles';
-import Clipboard from '@react-native-community/clipboard';
 
 const SignInComponent = ({signIn}: {signIn: () => void}) => {
   const fadeAnim = useRef(new Animated.Value(0.5)).current;
-  const [serverURL, setServerURL] = useState('');
-
-  const handlePaste = async () => {
-    const text = await Clipboard.getString();
-    setServerURL(text);
-  };
 
   Animated.loop(
     Animated.sequence([
@@ -38,16 +31,6 @@ const SignInComponent = ({signIn}: {signIn: () => void}) => {
         color={GoogleSigninButton.Color.Dark}
         onPress={signIn}
       />
-      <TextInput
-        style={styles.input}
-        placeholderTextColor={'#565656'}
-        placeholder="Enter Server URL"
-        autoCapitalize="none"
-        autoCorrect={false}
-        onChangeText={setServerURL}
-        value={serverURL}
-      />
-      <Button title="Paste" onPress={handlePaste} />
     </View>
   );
 };
